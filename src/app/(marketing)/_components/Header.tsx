73266ec66932a538b0ed32fc15ff605399c56fc9
joeltheai <joeltheai@users.noreply.github.com>
2024-03-08 14:28:06 +0530

    added header svg

--- src/app/(marketing)/_components/Header.tsx
+++ src/app/(marketing)/_components/Header.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
 const Header = () => {
   return (
     <header className="bg-red-700 p-4 text-white">
       <div className="container mx-auto">
-        <h1 className="text-2xl font-bold">H.Ex</h1>
+        <Link href="/">
+          <Image
+            src="/logo_header.svg"
+            width={50}
+            height={40}
+            alt="Picture of the author"
+          />
+        </Link>
       </div>
     </header>
   );
