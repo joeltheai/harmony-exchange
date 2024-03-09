585604344dd1d3777aa17bb26262bb1e072ae629
joeltheai <joeltheai@users.noreply.github.com>
2024-03-09 15:05:08 +0530

    created teacher db and api to siplay all teachers

---
+++ src/app/api/teacher/[teacherId]/route.ts
@@ -0,0 +1,94 @@
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prisma";
+import { auth } from "@clerk/nextjs";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { teacherId: string } },
+) {
+  try {
+    const teacher = await prismadb.teacher.findUnique({
+      where: {
+        id: params.teacherId,
+      },
+    });
+
+    return NextResponse.json(teacher);
+  } catch (error) {
+    console.log("[TEACHER_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { teacherId: string } },
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!params.teacherId) {
+      return new NextResponse("teacher id is required", { status: 400 });
+    }
+
+    const teacher = await prismadb.teacher.delete({
+      where: {
+        id: params.teacherId,
+      },
+    });
+
+    return NextResponse.json(teacher);
+  } catch (error) {
+    console.log("[TEACHER_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { teacherId: string } },
+) {
+  try {
+    const { userId } = auth();
+
+    const body = await req.json();
+
+    const { name, skill } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!skill) {
+      return new NextResponse("skill is required", { status: 400 });
+    }
+
+    if (!params.teacherId) {
+      return new NextResponse("teacher id is required", { status: 400 });
+    }
+
+    const teacher = await prismadb.teacher.update({
+      where: {
+        id: params.teacherId,
+      },
+      data: {
+        name,
+        skill,
+      },
+    });
+
+    return NextResponse.json(teacher);
+  } catch (error) {
+    console.log("[TEACHER_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
--- src/app/api/teacher/route.ts
+++ src/app/api/teacher/route.ts
@@ -1,1 +1,54 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
 
+import prismadb from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+
+    const body = await req.json();
+
+    const { name, skill, description } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!skill) {
+      return new NextResponse("Skill is required", { status: 400 });
+    }
+
+    const teacher = await prismadb.teacher.create({
+      data: {
+        userId,
+        name,
+        skill,
+        description,
+      },
+    });
+
+    return NextResponse.json(teacher);
+  } catch (error) {
+    console.log("[TEACHERS_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } },
+) {
+  try {
+    const teachers = await prismadb.teacher.findMany();
+
+    return NextResponse.json(teachers);
+  } catch (error) {
+    console.log("[TEACHERS_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
